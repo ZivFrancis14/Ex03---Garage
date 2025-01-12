8ecf62447e32a57ce7bfa4a01f7166156d48bdc7
Ziv Francis <zivfr@mta.ac.il>
2025-01-12 16:30:18 +0200

    changes

--- Ex03.ConsoleUI/CarConsole.cs
+++ Ex03.ConsoleUI/CarConsole.cs
@@ -27,16 +27,49 @@ namespace Ex03.ConsoleUI
         {
            
         }
-        private void currentFuelQuantityInLiters()
+        private float currentFuelQuantityInLiters(float i_MaxFuelQuantity)
         {
+            float currentFuelQuantity = 0;
+            bool isValidInput = false;
 
+            while (isValidInput == false)
+            {
+                try
+                {
+                    string msg = string.Format("Please enter the current fuel quantity in liters (0 to {0}):", i_MaxFuelQuantity);
+                    Console.WriteLine(msg);
+                    string userInput = Console.ReadLine();
+                    if (float.TryParse(userInput, out currentFuelQuantity))
+                    {
+                        msg = "Error: Invalid input. Please enter a numeric value.";
+                        Console.WriteLine(msg);
+                        continue;
+                    }
+
+                    // בדיקת טווח הערכים
+                    if (currentFuelQuantity < 0 || currentFuelQuantity > i_MaxFuelQuantity)
+                    {
+                        msg = string.Format("Error: Fuel quantity is out of range. Allowed range: 0 - {0} liters.", i_MaxFuelQuantity);
+                        Console.WriteLine(msg);
+                        continue;
+                    }
+
+                    // אם הכל תקין, היציאה מהלולאה
+                    isValidInput = true;
+                }
+                catch (Exception ex)
+                {
+                    string msg = string.Format("Unexpected error: {0}", ex.Message);
+                    Console.WriteLine(msg);
+                }
+            }
+
+            return currentFuelQuantity;
         }
+
         private void currentBatteryStatus()
         {
 
         }
-
-
-
     }
 }
