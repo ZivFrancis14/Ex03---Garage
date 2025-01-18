5eedd55685ad4b0371b7df54a8d18dcea107e134
Ziv Francis <zivfr@mta.ac.il>
2025-01-18 17:04:47 +0200

    fix

--- Ex03.GarageLogic/GarageManager.cs
+++ Ex03.GarageLogic/GarageManager.cs
@@ -5,30 +5,30 @@ namespace Ex03.GarageLogic
 {
     public class GarageManager
     {
-        private readonly Dictionary<string, VehicleRecord> r_CurrentVehicleInGarage;
+        private Dictionary<string, VehicleRecord> m_CurrentVehicleInGarage;
 
         public GarageManager()
         {
-            r_CurrentVehicleInGarage = new Dictionary<string, VehicleRecord>();
+            m_CurrentVehicleInGarage = new Dictionary<string, VehicleRecord>();
         }
         public Dictionary<string, VehicleRecord> CurrentVehicleInGarage
         {
             get 
             { 
-                return r_CurrentVehicleInGarage; 
+                return m_CurrentVehicleInGarage; 
             }
         }
         public void AddNewVehicle(VehicleRecord i_NewVehicle)
         {
-            r_CurrentVehicleInGarage.Add(i_NewVehicle.Vehicle.LicencePlate, i_NewVehicle);
+            m_CurrentVehicleInGarage.Add(i_NewVehicle.Vehicle.LicencePlate, i_NewVehicle);
         }
         public void ChangeVehicleStatus(string i_LicencePlateNumber, eVehicleStatus i_NewStatus)
         {
-            r_CurrentVehicleInGarage[i_LicencePlateNumber].VehicleStatus = i_NewStatus;
+            m_CurrentVehicleInGarage[i_LicencePlateNumber].VehicleStatus = i_NewStatus;
         }
         public bool IsVehicleExisit(string i_LicencePlateNumber)
         {
-            return r_CurrentVehicleInGarage.ContainsKey(i_LicencePlateNumber);
+            return m_CurrentVehicleInGarage.ContainsKey(i_LicencePlateNumber);
         }
     }
 }
